e2673c29c2bdfc11723e8ebc21414ddab98b9922
Fran Blanco <arbust908@gmail.com>
2024-01-27 18:09:28 -0300

    fix: darkmode

--- app.vue
+++ app.vue
@@ -15,16 +15,20 @@ useHead({
 
 <style>
 /* https://twitter.com/alirdev/status/1734136001671643465 for full height safe on device */
-
 html,
 body,
 #__nuxt {
-  color-scheme: light dark;
+  
   min-height: -webkit-fill-available;
   @apply h-100svh m-0 p-0 scroll-smooth;
 }
 
-html.dark {
-  @apply bg-slate-900 text-slate-100;
+body {
+  color-scheme: light;
+  @apply bg-slate-100;
+}
+.dark body {
+  color-scheme: dark;
+  @apply bg-slate-700 text-slate-100;
 }
 </style>
---
+++ components/bloby/Two.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+  
+</script>
+
+<template>
+  <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+  <path fill="#FF0066" d="M46.8,-15.4C55.8,12.5,55,43.5,37,58.2C19.1,73,-15.9,71.5,-38.6,54.6C-61.3,37.8,-71.8,5.5,-63.3,-21.7C-54.9,-49,-27.4,-71.2,-4.3,-69.8C18.9,-68.4,37.8,-43.4,46.8,-15.4Z" transform="translate(100 100)" />
+</svg>
+</template>
+
+<style scoped>
+    
+</style>
