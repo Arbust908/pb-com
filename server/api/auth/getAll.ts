3160e2a1b8d2daf6f4f80d432acf5e07208b5ef9
Fran Blanco <arbust908@gmail.com>
2024-01-26 23:08:59 -0300

    feat: chat ui update

--- server/api/auth/getAll.ts
+++ server/api/auth/getAll.ts
@@ -1,16 +1,20 @@
 import { defineEventHandler } from 'h3'
-import { redisService } from '@/server/redis-service'
+import { ToDoType } from '~/types'
+
+const getAllData = async () => {
+  return [{ username: 'admin', password: 'admin', email: 'admin@email.com' }]
+}
 
 export default defineEventHandler(async () => {
   try {
-    const all = await redisService.getAllData()
+    const all = await getAllData()
 
     if (!all)
       return { error: 'No data' }
 
     return { success: true, all }
   }
-  catch (error) {
+  catch (error: ToDoType) {
     return { error: error.message }
   }
 })
--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,4 @@
 export * from '@/types/AiModels'
 export * from '@/types/User'
+
+export type ToDoType = any
