49fa63c84e86437752a3fb8b0198aca4965a3ac5
Fran Blanco <arbust908@gmail.com>
2024-01-29 15:52:53 -0300

    chore: home fix

--- components/DotHero.vue
+++ components/DotHero.vue
@@ -6,7 +6,13 @@
 import { ref, onMounted } from 'vue';
 
 const canvas = ref<HTMLCanvasElement | null>(null);
-
+/* 
+1. draw a canvas with all your dots (dots, should be 2px by 2px and a ref variable so it can be changed and should be separated by 8px on both axis)
+2. generate a perlin noise (i used http://kitfox.com/projects/perlinNoiseMaker/)
+3. add `mask-image: url('/perlin-noise.webp');` to the canvas
+4. add keyframes to translate the position of the perlin noise (`-webkit-mask-position: 0px 0px`)
+Remember to to use Vue3, Script Setup Syntax and lang='ts'
+*/
 onMounted(() => {
     if (canvas.value) {
         const ctx = canvas.value.getContext('2d');
