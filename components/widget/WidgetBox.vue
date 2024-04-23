f0f4af625075241291ce03f8674794cf7e4f58bc
Fran Blanco <arbust908@gmail.com>
2024-04-23 07:56:14 -0300

    feat: phone widgets

---
+++ components/widget/WidgetBox.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+const route = useRoute();
+const isLong = computed(() => {
+    const query = route.query
+    const longMode = query.long_mode
+    return longMode;
+})
+</script>
+
+<template>
+     <div class="border-4 border-black rounded w-360px" :class="isLong ? 'aspect-ratio-2/1' : 'aspect-ratio-square'">
+        <slot :is-long="isLong"></slot>
+     </div>
+</template>
---
+++ pages/widget/index.vue
@@ -0,0 +1,23 @@
+<script setup lang='ts'>
+import type { RouteLocationRaw } from '#vue-router';
+import type { RouteRecordNormalized } from '#vue-router';
+
+const router = useRouter()
+function isWidget(route: RouteRecordNormalized) {
+    const { path } = route
+    // looks for pages that are widgets but nor the index
+    return path.startsWith('/widget') && path !== '/widget'
+}
+const widgetRoutes = router.getRoutes().filter(isWidget)
+function toLongPath(route: RouteRecordNormalized) {
+    return { path: route.path, query: { long_mode: true } } satisfies RouteLocationRaw
+}
+</script>
+<template>
+    <main class="grid content-center justify-items-center">
+        <div v-for="route in widgetRoutes" :key="route.path" class="grid md:grid-cols-2 gap-4">
+            <NuxtLink :to="route.path" class="text-xl md:text-sm font-bold rounded-md bg-purple-700 px-4 py-2 hover:(shadow bg-purple-800 text-gray-200)">{{ route.name }}</NuxtLink>
+            <NuxtLink :to="toLongPath(route)" class="text-xl md:text-sm font-bold rounded-md bg-purple-700 px-4 py-2 hover:(shadow bg-purple-800 text-gray-200)">{{ route.name }} Long</NuxtLink>
+        </div>
+    </main>
+</template>
