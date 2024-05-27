5cf052afae4477bf7ecc383c58c6a49bad79b32c
Fran Blanco <arbust908@gmail.com>
2024-05-27 15:12:04 -0300

    fix: chat text

--- components/chat/message.vue
+++ components/chat/message.vue
@@ -19,10 +19,10 @@ const timeAgo = useTimeAgo(props.timestamp || new Date())
 <template>
   <article class="flex items-start gap-2.5" :class="isUser && 'flex-row-reverse text-right'">
     <img class="w-8 h-8 rounded bg-slate-1 object-contain object-top-center" :src="avatar" :alt="`Avatar for ${role}`">
-    <div class="flex flex-col w-full max-w-[320px] leading-1.5">
+    <div class="flex flex-col w-full max-w-xs leading-1.5">
       <span class="text-xs font-normal text-gray-500 dark:text-gray-400">{{ timeAgo }}</span>
       <i v-if="!isUser && isResponding" class="i-svg-spinners:3-dots-fade mx-4 size-5" />
-      <p v-else class="text-sm font-normal pb-2 text-gray-900 dark:text-white">
+      <p v-else class="text-sm font-normal pb-2 text-gray-900 dark:text-white break-phrase text-stable">
         {{ text }}
       </p>
     </div>
