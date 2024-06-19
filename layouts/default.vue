2f4d831b6c577edff6f507b5f45057b3ffdd864e
Fran Blanco <arbust908@gmail.com>
2024-06-19 09:43:22 -0300

    feat: panchimio v1 + roller

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,11 +6,10 @@ const { print_mode, layout_start } = storeToRefs(general_store)
 <template>
   <div
     :class="[print_mode ? 'print pt-0' : null]"
-    :style="layout_start ? 'align-content: flex-start;' : null"
     class="grid grid-cols-[100%] grid-rows-[auto_1fr_auto] min-h-full"
   >
     <GeneralNavBar class="relative z-40" />
-    <main class="relative z-20 layout-grid">
+    <main class="relative z-20 layout-grid" :style="layout_start ? 'align-content: flex-start;' : null">
       <slot />
     </main>
     <GeneralFooter class="relative z-40" />
