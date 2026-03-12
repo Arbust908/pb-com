b05f3ed5a51a3521e7d677c0f19381df58d46250
Fran Blanco <arbust908@gmail.com>
2026-03-12 15:26:03 -0300

    fix: portfolio msg

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -16,37 +16,60 @@ const messages = [
   'Stay Tuned',
   'Something Awesome',
 ]
+const workingOn = [
+  'Full Next.ts rewrite',
+  'Versatile eCommerce with payment integration',
+  'Full Redesign and re-write in Svelte and Solid',
+  'The Tabletop Roleplaying Game Database',
+]
 
 const messageRef = ref<HTMLElement>()
-const currentIndex = ref(0)
+const workOnRef = ref<HTMLElement>()
+const messageIndex = ref(0)
+const workingOnIndex = ref(0)
+
+const shuffleIntervalOne = ref<ReturnType<typeof setInterval>>()
+const shuffleIntervalTwo = ref<ReturnType<typeof setInterval>>()
 
 onMounted(() => {
   if (messageRef.value) {
     shuffleLetters(messageRef.value)
+    shuffleLetters(workOnRef.value)
   }
 
-  setInterval(() => {
-    currentIndex.value = (currentIndex.value + 1) % messages.length
+  shuffleIntervalOne.value = setInterval(() => {
+    messageIndex.value = (messageIndex.value + 1) % messages.length
     if (messageRef.value) {
       shuffleLetters(messageRef.value, {
-        text: messages[currentIndex.value],
+        text: messages[messageIndex.value],
       })
     }
-  }, 3000)
+  }, 2560)
+  shuffleIntervalTwo.value = setInterval(() => {
+    workingOnIndex.value = (workingOnIndex.value + 1) % workingOn.length
+    if (workOnRef.value) {
+      shuffleLetters(workOnRef.value, {
+        text: workingOn[workingOnIndex.value],
+      })
+    }
+  }, 3725)
 })
 </script>
 
 <template>
-  <div class="h-screen flex flex-col items-center justify-center bg-slate-100 dark:bg-slate-900">
-    <div class="text-center space-y-6">
+  <div class="mt-40 min-h-screen flex flex-col items-center justify-start bg-slate-100 dark:bg-slate-900">
+    <div class="w-full text-center space-y-6">
       <h1 class="from-pink-500 to-violet-500 bg-gradient-to-r bg-clip-text text-5xl text-transparent font-light md:text-7xl">
         Portfolio
       </h1>
-      <p ref="messageRef" class="min-h-32px text-xl text-slate-600 font-medium tracking-widest dark:text-slate-400 md:text-2xl">
-        Coming Soon
+      <p ref="messageRef" class="min-h-32px text-xl text-slate-600 font-medium tracking-widest md:text-2xl dark:text-slate-400">
+        Working on it!
+      </p>
+      <p ref="workOnRef" class="min-h-32px text-slate-700 font-bold dark:text-slate-300">
+        Deep typescript tighting
       </p>
       <NuxtLink
-        class="inline-block rounded-lg bg-violet-400 px-6 py-2 text-slate-900 font-medium transition duration-150 ease-out dark:bg-violet-600 dark:text-slate-100 hover:text-violet-500 hover:shadow dark:hover:bg-violet-800"
+        class="inline-block rounded-lg bg-transparent px-6 py-2 text-slate-900 font-medium underline underline-offset-2 transition duration-150 ease-out dark:text-slate-100 hover:text-violet-200 hover:shadow dark:hover:bg-violet-800"
         to="/"
       >
         ← Back to Home
